f88a1a739a52d23d9c8d9c184426984d6cb1c54a
Lukas Czerner <lczerner@loguj.cz>
2012-12-04 10:56:43 +0100

    locale: Regenerate new messages.pot
    Fix gettext.sh script to generate messages.pot file. Add few comments
    into the gettext.sh and regenerate messages.pot

--- locales/gettext.sh
+++ locales/gettext.sh
@@ -1,9 +1,10 @@
 #/usr/bin/env bash
+# Generate messages.pot
 
-xgettext --language=python --copyright-holder='Koonsolo' --package-name='monorail' --package-version='1.0.0' --msgid-bugs-address='koen@koonsolo.com' -d monorail -o messages.pot ../source/*.py ../source_demo/*.py
+xgettext --language=python --copyright-holder='Koonsolo' --package-name='monorail' --package-version='1.0.0' --msgid-bugs-address='koen@koonsolo.com' -d monorail -o messages.pot ../monorail/*.py
 
-msginit --locale=ru_RU -o ru_RU.po
-msginit --locale=en_US -o en_US.po
-
-
+# Examples to create your own locale file
+#
+# msginit --locale=ru_RU -o ru_RU.po
+# msginit --locale=en_US -o en_US.po
 # msgfmt -o locale/ru_RU/LC_MESSAGES/monorail.mo ru_RU.po
